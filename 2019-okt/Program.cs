bc238d741e68cad9e42e036ef1122d1304c49e14
ATI9590 <65565903+ATI9590@users.noreply.github.com>
2023-05-17 21:14:04 +0200

    2019. okt & 2020. okt
    Idegesítő, de tanulságos feladatsorok

---
+++ 2019-okt/Program.cs
@@ -0,0 +1,111 @@
+﻿class bérlet
+{
+    public int megálló;
+    public string idő;
+    public int id;
+    public string típus;
+    public string érvényesség;
+}
+class eutazas
+{
+    static int napokszama(int e1, int h1, int n1, int e2, int h2, int n2)
+    {
+        h1 = (h1 + 9) % 12;
+        e1 = e1 - h1/10;
+        int d1 = 365*e1 + e1/4 - e1/100 + e1/400 + (h1*306 + 5)/10 + n1 - 1;
+        h2 = (h2 + 9) % 12;
+        e2 = e2 - h2/10;
+        int d2 = 365*e2 + e2/4 - e2/100 + e2/400 + (h2*306 + 5)/10 + n2 - 1;
+        return d2 - d1;
+    }
+    static DateTime idő(string input)
+    {
+        int év = Convert.ToInt32(input[0..4]);
+        int hó = Convert.ToInt32(input[4..6]);
+        int nap = Convert.ToInt32(input[6..8]);
+        DateTime x = new DateTime(év, hó, nap);
+        return x;
+    }
+    static void Main()
+    {
+        bérlet[] tömb = new bérlet[2000];
+        for (int i = 0; i < 2000; i++)
+            tömb[i] = new bérlet();
+        string[] be = File.ReadAllLines("utasadat.txt");
+        int hossz = be.Length;
+        for (int i = 0; i < hossz; i++)
+        {
+            string[] split = be[i].Split(' ');
+            tömb[i].megálló = Convert.ToInt32(split[0]);
+            tömb[i].idő = split[1];
+            tömb[i].id = Convert.ToInt32(split[2]);
+            tömb[i].típus = split[3];
+            tömb[i].érvényesség = split[4];
+        }
+        Console.WriteLine(tömb[0].idő);
+        //2.
+        Console.WriteLine("2. feladat");
+        Console.WriteLine("A buszra {0} utas akart felszállni\n", hossz);
+        //3.
+        Console.WriteLine("3. feladat");
+        int db = 0;
+        for (int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].érvényesség == "0" || (Convert.ToInt32(tömb[i].érvényesség) > 11 && idő(tömb[i].idő) >= idő(tömb[i].érvényesség)))
+            {
+                db++;
+            }
+        }
+        Console.WriteLine("A buszra {0} utas nem szállhatott fel.\n", db);
+        //4.
+        Console.WriteLine("4. feladat");
+        int max = 0;
+        Dictionary<int, int> megállók = new Dictionary<int, int>();
+        for (int i = 0; i < 30; i++)
+        {
+            db = 0;
+            for (int j = 0; j < hossz; j++)
+            {
+                if (tömb[j].megálló == i)
+                    db++;
+            }
+            if (max < db)
+                max = db;
+                megállók.Add(i, db);
+        }
+        for (int i = 0; i < 30; i++)
+        {
+            if (max == megállók[i])
+            {
+                Console.WriteLine("A legtöbb utas ({0} fő) a {1}. megállóban próbált felszállni.\n", max, i);
+                break;
+            }
+        }
+        //5.
+        Console.WriteLine("5. feladat");
+        int kedvezményes = 0;
+        int ingyenes = 0;
+        for (int i = 0; i < hossz; i++)
+        {
+            if ((Convert.ToInt32(tömb[i].érvényesség) > 0 && Convert.ToInt32(tömb[i].érvényesség) < 11) || (Convert.ToInt32(tömb[i].érvényesség) > 11 && idő(tömb[i].idő) <= idő(tömb[i].érvényesség)))
+            {
+                if (tömb[i].típus == "TAB" || tömb[i].típus == "NYB")
+                    kedvezményes++;
+                if (tömb[i].típus == "NYP" || tömb[i].típus == "RVS" || tömb[i].típus == "GYK")
+                    ingyenes++;
+            }
+        }
+        Console.WriteLine("Ingyenesen utazók száma: {0} fő", ingyenes);
+        Console.WriteLine("A kedvezményesen utazók száma: {0} fő\n", kedvezményes);
+        //7.
+        StreamWriter figyelmeztetés = new StreamWriter("figyelmeztetes.txt");
+        for (int i = 0; i < hossz; i++)
+        {
+            if (Convert.ToInt32(tömb[i].érvényesség) > 11 && napokszama(idő(tömb[i].idő).Year, idő(tömb[i].idő).Month, idő(tömb[i].idő).Day, idő(tömb[i].érvényesség).Year, idő(tömb[i].érvényesség).Month, idő(tömb[i].érvényesség).Day) <= 3)
+            {
+                figyelmeztetés.WriteLine("{0} {1}-{2}-{3}", tömb[i].id, idő(tömb[i].érvényesség).Year, idő(tömb[i].érvényesség).Month, idő(tömb[i].érvényesség).Day);
+            }
+        }
+        figyelmeztetés.Close();
+    }
+}
---
+++ 2020-okt/Program.cs
@@ -0,0 +1,154 @@
+﻿class Sorozat
+{
+    public string adás;
+    public string cím;
+    public int évad;
+    public int epizód;
+    public int hossz;
+    public bool megtekintve;
+}
+class sorozatok
+{
+    static DateTime dátum(string input)
+    {
+        string[] be = input.Split('.');
+        int év = Convert.ToInt32(be[0]);
+        int hónap = Convert.ToInt32(be[1]);
+        int nap = Convert.ToInt32(be[2]);
+        DateTime dátum = new DateTime(év, hónap, nap);
+        return dátum;
+    }
+    static string Hetnapja(int ev, int ho, int nap)
+    {
+        string[] napok = {"v", "h", "k", "sze", "cs", "p", "szo"};
+        int[] honapok = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+        if (ho < 3)
+            ev -= 1;
+        return napok[(ev + (ev / 4) - (ev / 100) + (ev / 400) + honapok[ho - 1] + nap) % 7];
+    }
+    static void Main()
+    {
+        Sorozat[] tömb = new Sorozat[400];
+        for (int i = 0; i < 400; i++)
+            tömb[i] = new Sorozat();
+        string[] be = File.ReadAllLines("lista.txt");
+        for (int i = 0; i < (be.Length / 5); i++)
+        {
+            string[] split = new string[5];
+            for (int j = 0; j < 5; j++)
+                split[j] = be[5*i + j];
+            if (split[0] != "NI")
+                tömb[i].adás = split[0];
+            if (split[0] == "NI")
+                tömb[i].adás = "0";
+            tömb[i].cím = split[1];
+            string[] x = split[2].Split("x");
+            tömb[i].évad = Convert.ToInt32(x[0]);
+            tömb[i].epizód = Convert.ToInt32(x[1]);
+            tömb[i].hossz = Convert.ToInt32(split[3]);
+            if (split[4] == "0")
+                tömb[i].megtekintve = false;
+            if (split[4] == "1")
+                tömb[i].megtekintve = true;
+        }
+        //2.
+        Console.WriteLine("2.feladat");
+        int hossz = be.Length / 5;
+        int db = 0;
+        for (int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].adás != "0")
+                db++;
+        }
+        Console.WriteLine("A listában {0} db vetítési dátummal rendelkező epizód van.\n", db);
+        //3.
+        Console.WriteLine("3. feladat");
+        db = 0;
+        for (int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].megtekintve == true)
+                db++;
+        }
+        Console.WriteLine("A listában lévő epizódok {0}%-át látta.\n", Math.Round((double)db / hossz * 100, 2));
+        //4.
+        Console.WriteLine("4. feladat");
+        int összidő = 0;
+        for (int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].megtekintve == true)
+            {
+                összidő += tömb[i].hossz;
+            }
+        }
+        int nap = összidő / 60 / 24;
+        int óra = összidő / 60 - (nap * 24);
+        int perc = összidő % 60;
+        Console.WriteLine("Sorozatnézéssel {0} napot {1} órát és {2} percet töltött.\n", nap, óra, perc);
+        //5.
+        Console.WriteLine("5. feladat");
+        Console.Write("Adjon meg egy dátumot! Dátum = ");
+        DateTime date = dátum(Console.ReadLine());
+        for (int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].adás != "0" && dátum(tömb[i].adás) <= date && tömb[i].megtekintve == false)
+            {
+                Console.WriteLine("{0}x{1}\t{2}", tömb[i].évad, tömb[i].epizód, tömb[i].cím);
+            }
+        }
+        Console.WriteLine();
+        //7.
+        Console.WriteLine("7. feladat");
+        Console.Write("Adja meg a hét egy napját (például cs)! Nap = ");
+        string hét = Console.ReadLine();
+        bool vane = false;
+        List<string> cím = new List<string>();
+        for(int i = 0; i < hossz; i++)
+        {
+            if (tömb[i].adás != "0" && Hetnapja(dátum(tömb[i].adás).Year, dátum(tömb[i].adás).Month, dátum(tömb[i].adás).Day) == hét)
+            {
+                for (int j = 0; j < cím.Count; j++)
+                {
+                    if (cím[j] == tömb[i].cím)
+                        vane = true;
+                }
+                if (vane == false)
+                    cím.Add(tömb[i].cím);
+                vane = false;
+            }
+        }
+        for (int i = 0; i < cím.Count; i++)
+            Console.WriteLine(cím[i]);
+        Console.WriteLine();
+        //8.
+        StreamWriter sum = new StreamWriter("summa.txt");
+        cím.Clear();
+        for (int i = 0; i < hossz; i++)
+        {
+            vane = false;
+            for (int j = 0; j < cím.Count; j++)
+            {
+                if (tömb[i].cím == cím[j])
+                    vane = true;
+            }
+            if (vane == false)
+                cím.Add(tömb[i].cím);
+        }
+        int y = 0;
+        int hossz2 = 0;
+        for (int i = 0; i < cím.Count; i++)
+        {
+            for (int j = 0; j < hossz; j++)
+            {
+                if (tömb[j].cím == cím[i])
+                {
+                    y++;
+                    hossz2 += tömb[i].hossz;
+                }
+            }
+            sum.WriteLine("{0} {1} {2}", cím[i], hossz2, y);
+            y = 0;
+            hossz2 = 0;
+        }
+        sum.Close();
+    }
+}
